f091336bd87c1dc54aa451549e14a6accad40e5b
J. TURNER <turner.j6@gmail.com>
2015-03-17 14:17:23 -0700

    Correct typo: 'presensce' --> 'presence.'

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 describe Lesson do
   it { should validate_presence_of :name }
-  it { should validate_presensce_of :content }
+  it { should validate_presence_of :content }
 end
