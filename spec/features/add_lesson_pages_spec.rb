15ad23b15835861432e31ae24a574d5858e53270
J. TURNER <turner.j6@gmail.com>
2015-03-17 12:35:15 -0700

    Add spec for 'add lesson' process.

---
+++ spec/features/add_lesson_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "add a lesson" do
+
+  it "adds a new lesson" do
+    visit lessons_path
+    click_on 'Add'
+    fill_in 'Name', :with => 'JavaScript Basics'
+    click_on 'Create'
+    expect(page).to have_content 'Lessons'
+  end
+
+  it "shows error if no name is entered" do
+    visit new_lession_path
+    click_on 'Create'
+    expect(page).to have_content 'errors'
+  end
+
+end
