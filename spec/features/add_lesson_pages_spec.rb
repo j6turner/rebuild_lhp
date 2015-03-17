7a72edfeee7342afa5abc6590e6d80a07ceb3bf1
J. TURNER <turner.j6@gmail.com>
2015-03-17 16:04:54 -0700

    Add 'content' component; pass spec.

--- spec/features/add_lesson_pages_spec.rb
+++ spec/features/add_lesson_pages_spec.rb
@@ -5,8 +5,10 @@ describe "add lesson" do
   it "adds a new lesson" do
     visit lessons_path
     click_on 'Create Lesson'
-    fill_in 'name', :with => 'Homework'
-    click_on 'Create'
+    fill_in 'Name', :with => 'JavaScript Basics'
+    click_on 'Create Lesson'
+    fill_in 'Content', :with => 'Syntax'
+    click_on 'Create Lesson'
     expect(page).to have_content 'Lessons'
   end
 
