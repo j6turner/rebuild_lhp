b080f73cc3db05ba6abb3ceb206c74f5b38553cb
J. TURNER <turner.j6@gmail.com>
2015-03-17 15:35:03 -0700

    Modify spec details; add 'Create Lesson' link.

--- spec/features/add_lesson_pages_spec.rb
+++ spec/features/add_lesson_pages_spec.rb
@@ -4,15 +4,15 @@ describe "add lesson" do
 
   it "adds a new lesson" do
     visit lessons_path
-    click_on 'Add'
-    fill_in 'Name', :with => 'JavaScript Basics'
+    click_on 'Create Lesson'
+    fill_in 'name', :with => 'Homework'
     click_on 'Create'
     expect(page).to have_content 'Lessons'
   end
 
   it "shows error if no name is entered" do
     visit new_lesson_path
-    click_on 'Create'
+    click_on 'Create Lesson'
     expect(page).to have_content 'errors'
   end
 
